fe71129b5500e0d0c7d6d5c4e5bf240084ae55c1
user1 <talivanov@gmail.com>
2018-06-07 17:23:26 +0300

    .

--- e2e-tests/scenarios.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-/* https://github.com/angular/protractor/blob/master/docs/toc.md */
-
-describe('my app', function() {
-
-});
---
+++ js/controllers/registration.js
@@ -0,0 +1,15 @@
+myApp.controller('RegistrationController', ['$scope',
+    function ($scope) {
+
+        $scope.part = "firstPart";
+
+        $scope.login = function () {
+        };
+
+        $scope.logout = function () {
+        };
+
+        $scope.register = function () {
+        };
+
+    }]);
